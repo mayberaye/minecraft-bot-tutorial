13ab8938d03fc5731dc1c6aea539e802dd9a158c
Raye <rayemayberry@yahoo.com>
2023-01-14 14:25:05 -0600

    load in bot

---
+++ main.js
@@ -0,0 +1,9 @@
+const mineflayer = require('mineflayer')
+
+const options = {
+    host: 'localhost', // Change this to the ip you want.
+    port: 34643 , // Change this to the port you want.
+    username: "Bot"
+  }
+  
+  const bot = mineflayer.createBot(options)
